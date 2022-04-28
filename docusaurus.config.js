9b5aa28bebab8b892c150cc116cddfe062898913
Daniel Esteves <me+github@danestves.com>
2022-04-27 21:31:48 -0400

    feat: adding eslint and prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,86 +1,86 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 const themeConfig = {
   liveCodeBlock: {
-    playgroundPosition: "bottom",
+    playgroundPosition: 'bottom',
   },
   navbar: {
-    title: "react-square-web-payments-sdk",
+    title: 'react-square-web-payments-sdk',
     logo: {
-      alt: "react-square-web-payments-sdk",
-      src: "img/logo.png",
+      alt: 'react-square-web-payments-sdk',
+      src: 'img/logo.png',
     },
     items: [
       {
-        type: "doc",
-        docId: "getting-started",
-        position: "left",
-        label: "Docs",
+        type: 'doc',
+        docId: 'getting-started',
+        position: 'left',
+        label: 'Docs',
       },
-      { to: "/blog", label: "Blog", position: "left" },
+      { to: '/blog', label: 'Blog', position: 'left' },
       {
-        href: "https://github.com/weareseeed/react-square-web-payments-sdk",
-        label: "GitHub",
-        position: "right",
+        href: 'https://github.com/weareseeed/react-square-web-payments-sdk',
+        label: 'GitHub',
+        position: 'right',
       },
     ],
   },
   footer: {
-    style: "light",
+    style: 'light',
     links: [
       {
-        title: "Docs",
+        title: 'Docs',
         items: [
           {
-            label: "Docs",
-            to: "/docs",
+            label: 'Docs',
+            to: '/docs',
           },
         ],
       },
       {
-        title: "Company",
+        title: 'Company',
         items: [
           {
-            label: "GitHub",
-            href: "https://github.com/weareseeed",
+            label: 'GitHub',
+            href: 'https://github.com/weareseeed',
           },
           {
-            label: "About",
-            href: "https://seeed.us/team/",
+            label: 'About',
+            href: 'https://seeed.us/team/',
           },
         ],
       },
       {
-        title: "Community",
+        title: 'Community',
         items: [
           {
-            label: "Instagram",
-            href: "https://www.instagram.com/seeedsocial/",
+            label: 'Instagram',
+            href: 'https://www.instagram.com/seeedsocial/',
           },
           {
-            label: "LinkedIn",
-            href: "https://www.linkedin.com/company/seeedus/",
+            label: 'LinkedIn',
+            href: 'https://www.linkedin.com/company/seeedus/',
           },
           {
-            label: "Twitter",
-            href: "https://twitter.com/seeedsocial",
+            label: 'Twitter',
+            href: 'https://twitter.com/seeedsocial',
           },
           {
-            label: "Facebook",
-            href: "https://www.facebook.com/seeedsocial",
+            label: 'Facebook',
+            href: 'https://www.facebook.com/seeedsocial',
           },
         ],
       },
     ],
     logo: {
-      alt: "react-square-web-payments-sdk logo",
-      src: "img/logo.png",
-      href: "https://react-square-payments.weareseeed.com",
+      alt: 'react-square-web-payments-sdk logo',
+      src: 'img/logo.png',
+      href: 'https://react-square-payments.weareseeed.com',
       width: 48,
     },
     copyright: `Copyright © ${new Date().getFullYear()} Seeed, LLC — Florida, USA`,
@@ -95,46 +95,45 @@ const themeConfig = {
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "react-square-web-payments-sdk",
-  tagline:
-    "Easily create PCI-compliant inputs to accept payments online with the Square Payments SDK",
-  url: "http://react-square-payments.weareseeed.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "weareseeed",
-  projectName: "rswps-site",
-  deploymentBranch: "gh-pages",
+  title: 'react-square-web-payments-sdk',
+  tagline: 'Easily create PCI-compliant inputs to accept payments online with the Square Payments SDK',
+  url: 'http://react-square-payments.weareseeed.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'weareseeed',
+  projectName: 'rswps-site',
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         blog: {
-          editUrl: "https://github.com/weareseeed/rswps-site/tree/main/blog/",
+          editUrl: 'https://github.com/weareseeed/rswps-site/tree/main/blog/',
           showReadingTime: true,
         },
         docs: {
-          editUrl: "https://github.com/weareseeed/rswps-site/tree/main/docs/",
-          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: 'https://github.com/weareseeed/rswps-site/tree/main/docs/',
+          sidebarPath: require.resolve('./sidebars.js'),
         },
         sitemap: {
-          changefreq: "weekly",
+          changefreq: 'weekly',
           priority: 0.5,
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
   ],
 
   plugins: [
-    "@docusaurus/theme-live-codeblock",
+    '@docusaurus/theme-live-codeblock',
     [
-      "@docusaurus/plugin-ideal-image",
+      '@docusaurus/plugin-ideal-image',
       {
         disableInDev: false,
       },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,24 @@
-import React from "react";
-import clsx from "clsx";
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import styles from "./index.module.css";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+// Dependencies
+import * as React from 'react';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+
+// Internals
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
+          <Link className="button button--secondary button--lg" to="/docs/intro">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
@@ -27,11 +28,10 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="The Web Payments SDK React wrapper lets you easily create PCI-compliant inputs to accept payments online with the Web Payments SDK."
+      title="Create PCI-compliant inputs to accept payments online"
     >
       <HomepageHeader />
       <main>
