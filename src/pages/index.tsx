07c3cc091d02635985c25c0efa5a66e61affd8e2
Daniel Esteves <me+github@danestves.com>
2022-04-27 21:35:23 -0400

    fix: broken link using hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 // Dependencies
 import * as React from 'react';
 import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
@@ -18,7 +19,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
+          <Link className="button button--secondary button--lg" to={useBaseUrl('/docs')}>
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
