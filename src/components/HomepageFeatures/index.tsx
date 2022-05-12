06b2b75686c2c44960ea95666929cb79a7083597
Daniel Esteves <me+github@danestves.com>
2022-05-12 11:30:40 -0400

    docs: removing features descriptions

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -17,34 +17,19 @@ const FeatureList: FeatureItem[] = [
     id: 'seeed',
     title: 'Created by',
     Svg: require('@site/static/img/seeed-imagotype.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and used to get your website up and running
-        quickly.
-      </>
-    ),
+    description: <></>,
   },
   {
     id: 'square',
     title: 'Approved by',
     Svg: require('@site/static/img/square-imagotype.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your docs into the{' '}
-        <code>docs</code> directory.
-      </>
-    ),
+    description: <></>,
   },
   {
     id: 'react',
     title: 'Powered by',
     Svg: require('@site/static/img/react-logo.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the same
-        header and footer.
-      </>
-    ),
+    description: <></>,
   },
 ];
 
